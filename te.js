fb1df95859db0b39c61f7fbfee72e14b090c93ec
Owen Stelmarski <hypedsupremelife@gmail.com>
2025-01-24 22:28:53 -0600

    fixes

---
+++ te.js
@@ -0,0 +1,26 @@
+fetch("https://x.com/i/api/2/notifications/all.json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&include_ext_is_blue_verified=1&include_ext_verified_type=1&include_ext_profile_image_shape=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_ext_limited_action_results=true&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_ext_views=true&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&include_ext_sensitive_media_warning=true&include_ext_trusted_friends_metadata=true&send_error_codes=true&simple_quoted_tweet=true&count=20&requestContext=launch&ext=mediaStats%2ChighlightedLabel%2CparodyCommentaryFanLabel%2CvoiceInfo%2CbirdwatchPivot%2CsuperFollowMetadata%2CunmentionInfo%2CeditControl%2Carticle", {
+    "headers": {
+      "accept": "*/*",
+      "accept-language": "en-US,en;q=0.9",
+      "authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Not A(Brand\";v=\"8\", \"Chromium\";v=\"132\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-client-transaction-id": "J1IbTNXACv34k3xPoNz7MqKoLvFJlwrLACWvALruxmICfLFRm/+Fx590LpKbgBAx39okYiSkj9Ps+PWZIxxxeqdb0HWNJA",
+      "x-client-uuid": "ec88267e-bfd6-4f4f-9175-20d8ffb5a066",
+      "x-csrf-token": "7e658ae6c2ef040770e5bd59fc267aff07bf408ab3514630164590c8d0589cb930a25e4b2576dd16a6a06d579565d91af9cf53ec0ee4e94ad068a1562561c57696b54912840e572025b112b2fbcd9157",
+      "x-twitter-active-user": "yes",
+      "x-twitter-auth-type": "OAuth2Session",
+      "x-twitter-client-language": "en",
+      "x-twitter-polling": "true",
+      "cookie": "lang=en; kdt=QqLrDEE3Z7ttNOQU2cnmA5GhTZk8WTfDrl8WJlap; _tracking_consent=%7B%22con%22%3A%7B%22CMP%22%3A%7B%22a%22%3A%22%22%2C%22m%22%3A%22%22%2C%22p%22%3A%22%22%2C%22s%22%3A%22%22%7D%7D%2C%22v%22%3A%222.1%22%2C%22region%22%3A%22USTX%22%2C%22reg%22%3A%22%22%2C%22purposes%22%3A%7B%22a%22%3Atrue%2C%22p%22%3Atrue%2C%22m%22%3Atrue%2C%22t%22%3Atrue%7D%2C%22display_banner%22%3Afalse%2C%22sale_of_data_region%22%3Afalse%2C%22consent_id%22%3A%22E708A820-2b28-4800-9f36-baa464cb2bf9%22%7D; _shopify_y=1780fada-80e2-4c75-97c1-38901aba63c9; intercom-device-id-jgtierkz=4630ae01-83f9-45cf-bf8f-c387ec3c56de; d_prefs=MjoxLGNvbnNlbnRfdmVyc2lvbjoyLHRleHRfdmVyc2lvbjoxMDAw; des_opt_in=Y; ph_phc_TXdpocbGVeZVm5VJmAsHTMrCofBQu3e0kN8HGMNGTVW_posthog=%7B%22distinct_id%22%3A%2201949afc-ffc2-7f22-bdc8-6ff383252030%22%2C%22%24sesid%22%3A%5B1737769003660%2C%2201949afc-ffc1-76fe-9846-04fc5972c99e%22%2C1737767059393%5D%7D; dnt=1; personalization_id=\"v1_0iPIE8eqbSkmnqBcvOxNPA==\"; g_state={\"i_l\":0}; auth_multi=\"1114611046407446529:1f9cb587f6f349c09c1ff0e5b7387dda9905a9e0\"; auth_token=fd8499dc83afd1da3727a4a33eb7a7d59118ea3d; guest_id_ads=v1%3A173777856850547212; guest_id_marketing=v1%3A173777856850547212; guest_id=v1%3A173777856850547212; twid=u%3D3181780159; ct0=7e658ae6c2ef040770e5bd59fc267aff07bf408ab3514630164590c8d0589cb930a25e4b2576dd16a6a06d579565d91af9cf53ec0ee4e94ad068a1562561c57696b54912840e572025b112b2fbcd9157; intercom-session-jgtierkz=THBkYkpBNUdudTdQbG1ROS9hTnA3V0EwK2RZTjc0NmErdHZMUDZyRmEwYnFETGNlRDV1aEY0ZzJIVHhxdElQZERXZ08vOURqVzE4cEtSdzNEZXZraHV2U0ZBL2x5elBiRld3MjlSYXNnNkE9LS1Bb2x6bDN4ZVhaRndNNU5EZlRPaC9BPT0=--e190b9dbc8ff138fc74f04e8ec8b1f18eb82842b",
+      "Referer": "https://x.com/notifications/mentions",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": null,
+    "method": "GET"
+  });
